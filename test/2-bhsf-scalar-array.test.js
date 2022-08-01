f08a3f6f89e14caa78e985f72495ef5cb5d5fe5c
patricio.quiroz <ext_pmquirozv@falabella.cl>
2022-08-01 10:17:46 -0400

    update test using arrayContaining

--- test/2-bhsf-scalar-array.test.js
+++ test/2-bhsf-scalar-array.test.js
@@ -18,7 +18,7 @@ describe("Tests Scalar Product Array", () => {
         expect(prodScalarArray(6, [5, 7, 8].sort())).toEqual([30, 42, 48].sort());
     });
 
-    test('when [1, 10, 7, 9, 17, 29] product 9', () => {
-        expect(prodScalarArray(20, [1, 10, 7, 9, 17, 29].sort())).toEqual([20, 200, 140, 180, 340, 580].sort());
+    test('when [1, 10, 7, 9, 17, 29] product 20', () => {
+        expect(prodScalarArray(20, [1, 10, 7, 9, 17, 29])).toEqual(expect.arrayContaining([20, 200, 140, 180, 340, 580]));
     });
 });
