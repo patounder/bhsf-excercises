36c554c6b31b80ddc991f39d0d579f90570ce324
patricio.quiroz <ext_pmquirozv@falabella.cl>
2022-08-01 10:18:31 -0400

    add new test to scalar prod vector

--- test/3-bhsf-scalar-vector.test.js
+++ test/3-bhsf-scalar-vector.test.js
@@ -1,6 +1,6 @@
 const handler = require('../src/3-bhsf-scalar-vector');
 
-const event = {
+const firstEvent = {
     headers: {},
     body: JSON.stringify({
         scalar: 2,
@@ -8,14 +8,40 @@ const event = {
     })
 }
 
+const secondEvent = {
+    headers: {},
+    body: JSON.stringify({
+        scalar: 5,
+        vector: [[2], [5], [0, 10]]
+    })
+}
+
+const emptyVectorEvent = {
+    headers: {},
+    body: JSON.stringify({
+        scalar: 9,
+        vector: [[],[]]
+    })
+}
+
 describe("Tests Scalar prod Vector", () => {
 
-    test('vector to double', () => {
-        return handler(event).then(data => {
+    test('when [[2, 2], [5, -1], [0, 10]] product 2', () => {
+        return handler(firstEvent).then(data => {
             expect(data).toEqual({
                 statusCode: 200,
                 body:`With scalar 2, vector [2,2,5,-1,0,10] then result vector [4,4,10,-2,0,20]`
             });
         });
     });
+
+    test('when [[],[]] product 9', () => {
+        return handler(emptyVectorEvent).then(data => {
+            expect(data).toEqual({
+                statusCode: 200,
+                body:`With scalar 9, vector [,] then result vector [,]`
+            });
+        });
+    });
+
 });
