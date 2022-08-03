441f4b0338634053364d472dab9f66c0dd8ea2e6
patricio.quiroz <ext_pmquirozv@falabella.cl>
2022-08-03 17:02:19 -0400

    remove unnecessary case border test

--- test/1-bhsf-fib.test.js
+++ test/1-bhsf-fib.test.js
@@ -3,18 +3,14 @@ const calcFibNumber = require('../src/1-bhsf-fib');
 describe("Tests Fibonacci Numbers", () => {
 
     test('when index 6', () => {
-        expect(calcFibNumber(6)).toBe(8);
+        expect(calcFibNumber(6, 1, 0)).toBe(8);
     });
 
     test('when index 11', () => {
-        expect(calcFibNumber(11)).toBe(89);
+        expect(calcFibNumber(11, 1, 0)).toBe(89);
     });
 
     test('when index 20', () => {
-        expect(calcFibNumber(20)).toBe(6765);
-    });
-
-    test('border case, when negative index -20', () => {
-        expect(calcFibNumber(-20)).toBe(0);
+        expect(calcFibNumber(20, 1, 0)).toBe(6765);
     });
 });
